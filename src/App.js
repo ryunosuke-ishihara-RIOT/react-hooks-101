79e189f957efe3def4b63330dcb2e040176dd6b4
ryunosuke-ishihara-RIOT <ryunosuke.ishihara8124@gmail.com>
2021-01-10 19:43:24 +0900

    implement component maltiple-state

--- src/App.js
+++ src/App.js
@@ -1,53 +1,30 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  // console.log({ count });
-  // console.log({ setCount });
-  // console.log(typeof setCount);
-  // console.log(typeof count);
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+  const increment = () => setPrice(price + 1);
 
-  const increment = () => setCount(count + 1);
-
-  const decriment = () => setCount(count - 1);
-
-  const increment2 = () => setCount((previousCount) => previousCount + 1);
-
-  const decrement2 = () => setCount((hoge) => hoge - 1);
-
-  const reset = () => setCount(0);
-
-  const double = () => setCount(count * 2);
-
-  const division3 = () =>
-    setCount((hoge) => {
-      if (hoge % 3 === 0) {
-        return hoge / 3;
-      } else {
-        return hoge;
-      }
-    });
-
-  const division4 = () =>
-    setCount((hoge) => (hoge % 4 === 0 ? hoge / 4 : hoge));
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  };
 
   return (
     <>
-      <div>count : {count}</div>
+      <p>
+        現在の{name}は、{price}円です
+      </p>
       <button onClick={increment}>+1</button>
-      <button onClick={decriment}>-1</button>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>reset</button>
-        <button onClick={double}>*2</button>
-        <button onClick={division3}>3の倍数の時だけ3で割る</button>
-        <button onClick={division4}>4の倍数の時だけ4で割る</button>
-      </div>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
     </>
   );
 };
 
+App.defaultProps = {
+  name: 'sample',
+  price: 1000,
+};
 export default App;
